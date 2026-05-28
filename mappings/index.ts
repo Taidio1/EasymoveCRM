271900f18f713076aa6ce32961121a7637beaf39
Kacper <kkacper15@gmail.com>
2026-05-28 20:34:24 +0200

    feat: add mapping registry and placeholder mapping for wniosek-pobyt-czasowy

---
+++ mappings/index.ts
@@ -0,0 +1,8 @@
+import type { DocumentTemplate } from "@/lib/document-types"
+
+export const DOCUMENT_TEMPLATES: DocumentTemplate[] = [
+  {
+    id: "wniosek-pobyt-czasowy",
+    name: "Wniosek o udzielenie zezwolenia na pobyt czasowy i pracę",
+  },
+]
